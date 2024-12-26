ceae849686872f0bd6fe81a1b0a2f474b85fe30c
krishhsuri <krish050805@gmail.com>
2024-12-27 02:04:55 +0530

    1:40:55

---
+++ src/app/(website)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LandingPageNavbar from './_components/navbar'
+
+type Props = {
+  children : React.ReactNode
+}
+
+const layout = ({children}: Props) => {
+  return (
+    <div className='flex flex-col py-10 px-10 xl:px-0 container'>
+      <LandingPageNavbar/>{children}</div>
+  )
+}
+
+export default layout
---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import { Manrope, DM_Sans } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import './globals.css'
+import { ThemeProvider } from '@/components/theme'
+import { Toaster } from 'sonner'
+
+const manrope = DM_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'SnapStream',
+  description: 'Share AI powered videos with your friends.',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${manrope.className} bg-[#171717]`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
+            
+                {children}
+                <Toaster />
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
